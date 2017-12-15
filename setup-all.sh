5810fec8ac685195bbd18a0e29226eab728e12d2
Hagen <hagen@serious-shit.de>
2017-12-15 18:39:11 +0100

    traefik

--- setup-all.sh
+++ setup-all.sh
@@ -2,4 +2,10 @@
 
 # setup or update all Docker Stacks
 
+#setup networks
+docker network create --driver=overlay loadbalancer-net
+
+#Portainer
 docker stack deploy -c portainer-docker-compose.yml portainer
+#Mailserver
+#docker stack deploy -c mailserver-docker-compose.yml mailserver
